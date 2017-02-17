438f81708696692fa2b75425d2f9660091f84bcc
yanling <yanlingw95@gmail.com>
2017-02-17 10:19:28 +0800

    test

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ renderTasks();
 var taskArr = getTask();
 
 //keydown event
-getItemById("add-task-input").onkeydown = function(event) {
-  // key "enter"
+listener(getItemById("add-task-input"), 'keydown', function(event) {
+    // key "enter"
   if(event.keyCode == 13) {
-  	if(this.value == ""){return;}
+    if(this.value == "") {
+      return;
+    }
     var content = this.value;
     this.value = "";
     var taskObj = {};   
@@ -15,7 +17,7 @@ getItemById("add-task-input").onkeydown = function(event) {
     addTask(taskObj);
     renderTasks();
   }
-}
+});
 
 //get all tasks
 function getTask() {
@@ -36,7 +38,7 @@ function addTask(taskObj) {
   if(taskArr.length === 0) {
     taskObj.id = 0;
   }else {
-    taskObj.id = taskArr[taskArr.length-1].id+1;
+    taskObj.id = taskArr[taskArr.length-1].id + 1;
   }
   taskArr.push(taskObj);
   updateTask(taskArr);
@@ -44,9 +46,9 @@ function addTask(taskObj) {
 
 //delete a task
 function deleteTask(e, id) {
-	for(var i = 0;i<taskArr.length;i++) { 
+	for(var i = 0; i<taskArr.length; i++) { 
 	  if(this.id == taskArr[i].id) {     
-	    taskArr.splice(i,1);	
+	    taskArr.splice(i, 1);	
 	    break;   
 	  }
   }
@@ -63,8 +65,8 @@ function editTask(e) {
   var id = e.parentNode.getAttribute("taskId");
   //if the new task content is null, the previous one remains
   editInputObj.onblur = function() {
-    e.innerHTML = this.value?this.value:previousTaskContent;
-    for(var i = 0;i<taskArr.length;i++) {
+    e.innerHTML = this.value? this.value: previousTaskContent;
+    for(var i = 0; i<taskArr.length; i++) {
       if(taskArr[i].id == id) {
         taskArr[i].content = editInputObj.value;
         break;
@@ -79,7 +81,7 @@ function editTask(e) {
 //change the state of item, depending on whether the item has completed or not
 function clickCheckbox(e, id) {
   var taskId = this.id; 
-  for(var i=0;i<taskArr.length;i++) {
+  for(var i=0; i<taskArr.length; i++) {
 	if(taskArr[i].id == taskId) {
 	  taskArr[i].finish =! taskArr[i].finish;	
 	}		 	   
@@ -117,21 +119,20 @@ function showFilterTaskList() {
     }
     renderTasks(displayedItems);
   };
-
   listener(allTasks, 'click', callback.bind(this, 'all'));
-  listener(activeTasks,'click',callback.bind(this, 'active'));
-  listener(completedTasks,'click',callback.bind(this, 'completed'));
-  listener(clearCompleted,'click',callback.bind(this, 'clearAllCompleted'));
+  listener(activeTasks, 'click',callback.bind(this, 'active'));
+  listener(completedTasks, 'click',callback.bind(this, 'completed'));
+  listener(clearCompleted, 'click',callback.bind(this, 'clearAllCompleted'));
 }
 showFilterTaskList();
 
 //add mark color to filter buttons when they are clicked
 var sortTask = getItemById('sort-task');
 var sortControls = sortTask.getElementsByTagName('a');
-for(var i = 0;i<sortControls.length;i++) {
+for(var i = 0; i<sortControls.length; i++) {
   sortControls[i].index = i;   
   sortControls[i].onclick = function() {
-    for(var j = 0;j<sortControls.length;j++) {
+    for(var j = 0; j<sortControls.length; j++) {
       if(j == this.index) {
         sortControls[j].style.color = "#339999";
       }else {
@@ -141,6 +142,7 @@ for(var i = 0;i<sortControls.length;i++) {
   }
 }
 
+  var taskList = getItemById('task-list');
 //render all tasks on to-do list
 function renderTasks(val) {
   var taskList = getItemById('task-list');
@@ -149,7 +151,7 @@ function renderTasks(val) {
   var completedList = '<ul id="completed-list">';
   var taskArr = val || getTask();
   var liContent;                      
-  for(var i = 0;i<taskArr.length;i++) {
+  for(var i = 0; i<taskArr.length; i++) {
     liContent = '<li '+'taskId="' + taskArr[i].id + '" '+'class="task"'+'>'
       +'<a '+'class="check"'+'id="'+taskArr[i].id+'"'+'onclick="clickCheckbox.call(this,id);"'+'>'+'O'+'</a>'
       +'<span class="task-content" ondblclick="editTask(this);">'+taskArr[i].content+'</span>'
@@ -177,3 +179,4 @@ function listener(node, type, callback) {
 function getItemById(id) {
   return document.getElementById(id);
 }
+
